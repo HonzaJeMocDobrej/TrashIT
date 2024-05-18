1d3ad670b0a125285316526141534e8ba05c356c
Jan Fedor <115611769+HonzaJeMocDobrej@users.noreply.github.com>
2024-05-18 21:44:59 +0200

    Everything should be required

--- server/models/uploads.js
+++ server/models/uploads.js
@@ -17,10 +17,10 @@ const uploadsSchema = mongoose.Schema({
         type: Number, required: true
     },
     imagePath: {
-        type: String, required: false
+        type: String, required: true
     },
     category: {
-        type: String, required: false
+        type: String, required: true
     },
     password: {
         type: String, required: true
